1328e76db860488456564897fc4970f1b7c6221e
Jérémy VIGNELLES <jeremy.vignelles@gmail.com>
2020-11-29 11:50:42 +0100

    Repro for null coalescing operator with generics

--- Program.cs
+++ Program.cs
@@ -2,6 +2,17 @@
 
 namespace repro_dotnet
 {
+    public class TestClass<T> {
+        public void DoStuff() {
+            string p = "";
+            var result = ((TestClass<T>?)null)?.DoOtherStuff(ref p);
+        }
+
+        public T? DoOtherStuff(ref string p) {
+            return default;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
